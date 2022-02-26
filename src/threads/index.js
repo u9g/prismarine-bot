e7667176c6fe526a859f47ad2c27f525ddb5bcd4
u9g <43508353+u9g@users.noreply.github.com>
2022-02-26 15:11:32 -0500

    Add config

--- src/threads/index.js
+++ src/threads/index.js
@@ -1,17 +1,17 @@
 const { Client } = require('discord.js')
 const { createThread } = require('./handle')
+const config = require('../../config')
 
 const channels = ['']
-const guild = ''
 
 /**
- * 
- * @param {Client} client 
+ *
+ * @param {Client} client
  */
 module.exports = async client => {
   client.on('messageCreate', async (msg) => {
     if (!msg || !msg.content) return
-    if (msg.author.bot || !channels.includes(msg.channelId) || msg.guildId !== guild || msg.channel.isThread()) return
+    if (msg.author.bot || !channels.includes(msg.channelId) || msg.guildId !== config.THREAD_HELP_CHANNEL || msg.channel.isThread()) return
     await createThread(msg)
   })
 }
