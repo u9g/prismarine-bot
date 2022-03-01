28291a011cbf56063b831abe285cd56468f16670
u9g <aibotted@gmail.com>
2022-02-28 19:16:51 -0500

    Actually close threads :)

--- src/threads/handle.js
+++ src/threads/handle.js
@@ -8,7 +8,7 @@ async function createThread (message) {
   const thread = await message.startThread({
     name: `${message.author.username} | Help`,
     autoArchiveDuration: 1440
-  }).catch(() => {})
+  })
 
   if (typeof thread === 'undefined') return
   await handleThread(thread, message)
@@ -27,32 +27,21 @@ async function handleThread (thread, message) {
     max: 1
   })
 
-  const closeCollector = thread.createMessageCollector({
-    filter: msg => msg && msg?.cleanContent === '!close' && msg.channelId === thread.id,
-    max: 1
-  })
-
   collector.once('collect', async (collected) => {
     const title = collected.cleanContent.match(titleRegex)
-    if (title == null || thread.deleted) return
+    const ogAuthor = await collected.channel.fetchStarterMessage().then(msg => msg.author)
+    if (title == null ||
+       thread.archived ||
+        collected.author !== ogAuthor) return
 
     await thread.edit({
       name: `${message.author.username} | ${title[1]}`
-    }).catch(() => {})
+    })
   })
 
   collector.once('end', async () => {
-    if (thread.deleted || !sent.editable) return
+    if (thread.archived || !sent.editable) return
     await editMessage(sent, message, false)
-  })
-
-  closeCollector.once('collect', async (msg) => {
-    await editMessage(sent, message, true)
-    try {
-      await msg.react('✅')
-      await thread.setArchived(true)
-      await message.react('✅')
-    } catch (error) {}
   })
 }
 
--- src/threads/index.js
+++ src/threads/index.js
@@ -5,6 +5,8 @@ const config = require('../../config')
 module.exports = async client => {
   /** @returns {Boolean} */
   async function userAlreadyHasOpenThread (user) {
+    return false // will be restored once discord stops throwing errors for correct code...
+    /*
     const channel = await client.channels.fetch(config.THREAD_HELP_CHANNEL, { cache: false, force: true })
     if (channel === null) return true
     if (channel.isText()) {
@@ -16,11 +18,21 @@ module.exports = async client => {
         }
       }
       return false
-    }
+    } */
   }
 
   client.on('messageCreate', async (msg) => {
-    if (!msg || !msg.content || msg.author.bot || msg.channelId !== config.THREAD_HELP_CHANNEL || await userAlreadyHasOpenThread(msg.author)) return
-    await createThread(msg)
+    if (!msg || !msg.content || msg.author.bot) return
+    if (msg.channelId === config.THREAD_HELP_CHANNEL && !(await userAlreadyHasOpenThread(msg))) {
+      await createThread(msg)
+    } else if (msg.channel.isThread()) {
+      const firstMessage = await msg.channel.fetchStarterMessage()
+      if (msg.content.includes('!close') && (firstMessage.author === msg.author || msg.member.roles.cache.toJSON().length > 0)) {
+        await msg.react('✅')
+        await msg.channel.setLocked(true)
+        await msg.channel.setArchived(true)
+        await firstMessage.react('✅')
+      }
+    }
   })
 }
